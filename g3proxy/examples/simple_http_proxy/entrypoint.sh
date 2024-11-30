d826cae5ab9c884143596d429fe7cf33b8b7e4d4
jiayuxin <jiayuxin@eacon.com>
2024-11-30 14:40:11 +0800

    更新 Dockerfile 和启动脚本以及相关证书

--- g3proxy/examples/simple_http_proxy/entrypoint.sh
+++ g3proxy/examples/simple_http_proxy/entrypoint.sh
@@ -2,10 +2,10 @@
 nacos=10.30.41.31
 
 
-curl "http://${nacos}:8848/nacos/v1/cs/configs?group=gmssl&dataId=server_enc.crt" - /usr/bin/server_enc.crt
+curl "http://${nacos}:8848/nacos/v1/cs/configs?group=gmssl&dataId=server_enc.crt" -o /usr/bin/server_enc.crt
 curl "http://${nacos}:8848/nacos/v1/cs/configs?group=gmssl&dataId=server_enc.key" -o /usr/bin/server_enc.key
-curl "http://${nacos}:8848/nacos/v1/cs/configs?group=gmssl&dataId=sever_sign.crt" -o /usr/bin/sever_sign.crt
-curl "http://${nacos}:8848/nacos/v1/cs/configs?group=gmssl&dataId=sever_sign.key" -o /usr/bin/sever_sign.key
+curl "http://${nacos}:8848/nacos/v1/cs/configs?group=gmssl&dataId=server_sign.crt" -o /usr/bin/server_sign.crt
+curl "http://${nacos}:8848/nacos/v1/cs/configs?group=gmssl&dataId=server_sign.key" -o /usr/bin/server_sign.key
 curl "http://${nacos}:8848/nacos/v1/cs/configs?group=gmssl&dataId=subca.crt" -o /usr/bin/chain-ca.crt
 curl "http://${nacos}:8848/nacos/v1/cs/configs?group=gmssl&dataId=ca.crt" >> /usr/bin/chain-ca.crt
 
